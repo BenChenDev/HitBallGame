c4c1cc69072ab33af1fe7df7c586ecd945552e99
TRUZijunChen <benjamin9527@icloud.com>
2019-03-07 14:34:39 -0800

    add destroy object

---
+++ Assets/Scripts/Destroy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    public GameObject hitBy;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ball" ) {
+            Debug.Log("Destroyed");
+            Destroy(this.gameObject);
+        }
+    }
+}
