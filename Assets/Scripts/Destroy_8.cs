0dc4a834a5fd4278c7b0ebfddee20f83496aa3c5
TRUZijunChen <benjamin9527@icloud.com>
2019-03-10 14:02:04 -0700

    add my eyes

--- Assets/Scripts/Destroy_8.cs
+++ Assets/Scripts/Destroy_8.cs
@@ -1,22 +1,25 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Destroy_8 : MonoBehaviour
 {
-    public AudioSource tickSource;
+    public AudioSource Audio;
     void Start()
     {
-        
+        Audio = GetComponent<AudioSource>();
+        if (Audio == null) {
+            Debug.Log("sound null.");
+        }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Ball")
-        {   tickSource = GetComponent<AudioSource>();
+        {
+            Audio.Play();
             Debug.Log("Destroyed");
             Destroy(this.gameObject);
-            tickSource.Play();
+            
             ScoreScript.scoreValue += 8;
         }
     }
