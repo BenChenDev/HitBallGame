873f2cb19959a21fd4c566cadb5cce6a76cdef38
TRUZijunChen <benjamin9527@icloud.com>
2019-03-15 21:22:14 -0700

    add user menu

---
+++ Assets/Scripts/GazeAtCount.cs
@@ -0,0 +1,63 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GazeAtCount : MonoBehaviour
+{
+
+    // Whether the Google Cardboard user is gazing at this button.
+    private bool isLookedAt = false;
+
+    // How long the user can gaze at this before the button is clicked.
+    public float timerDuration = 3f;
+
+    // Count time the player has been gazing at the button.
+    private float lookTimer = 0f;
+
+    // Graphical progress indicator.
+    private GameObject gazeTimer;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // While player is looking at this button.
+        if (isLookedAt)
+        {
+
+            // Increment the gaze timer.
+            lookTimer += Time.deltaTime;
+            
+
+            // Gaze time exceeded limit - button is considered clicked.
+            if (lookTimer > timerDuration)
+            {
+                lookTimer = 0f;
+
+                Debug.Log("Button selected!");
+                GetComponent<Button>().onClick.Invoke();
+            }
+        }
+
+        // Not gazing at this anymore, reset everything.
+        else
+        {
+            lookTimer = 0f;
+        }
+    }
+
+    // Record whether Google Cardboard user is gazing at the button.
+    // gazedAt: Set it to the value passed from event trigger.
+    public void SetGazedAt(bool gazedAt)
+    {
+        isLookedAt = gazedAt;
+    }
+
+}
---
+++ Assets/Scripts/Gazetest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Gazetest : MonoBehaviour
+{
+
+    public float gazeTime = 2f;
+    private float timer;
+    private bool gazeAt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gazeAt) {
+            timer += Time.deltaTime;
+            Debug.Log("timer: " + timer + ", gazetime: " + gazeTime);
+            if (timer >= gazeTime) {
+                boxMoveUp();
+                gazeAt = false;
+                timer = 0;
+            }
+        }
+        
+    }
+
+    private void boxMoveUp() {
+        transform.position += new Vector3(0f, 1f, 0f);
+    }
+
+    public void lookat() {
+        gazeAt = true;
+        
+    }
+}
